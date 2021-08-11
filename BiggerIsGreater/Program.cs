ba8d737bd568de3f0a9fe633ea6ad8f5e9679978
Jabrayilzade <cnixad@gamil.com>
2021-08-11 16:57:29 +0400

    Bigger is greater algorithm

---
+++ BiggerIsGreater/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using static System.Console;
+
+namespace BiggerIsGreater
+{
+    internal static class Program
+    {
+        private static bool IsAllCharsSame(IReadOnlyList<char> arr)
+        {
+            var isSame = true;
+            foreach (var t in arr)
+                if (t != arr[0])
+                    isSame = false;
+
+            return isSame;
+        }
+
+        private static bool IsPossibleWord(IReadOnlyList<char> arr)
+        {
+            if (arr.Count == 1) return false;
+            return !IsAllCharsSame(arr);
+        }
+
+
+        private static string Rearrange(char[] arr)
+        {
+            for (var i = arr.Length - 2; i >= 0; i--)
+            {
+                for (var j = arr.Length - 1; j >= i + 1; j--)
+                    if (string.CompareOrdinal(arr[j].ToString(), arr[i].ToString()) > 0)
+                    {
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                        if (j != arr.Length - 1) Array.Sort(arr, i, j);
+                        return new string(arr);
+                    }
+            }
+
+            return "no answer";
+        }
+
+        private static string BiggerIsGreater(string w)
+        {
+            var arr = w.ToCharArray();
+            var result = "no answer";
+
+            if (IsPossibleWord(arr))
+                result = Rearrange(arr);
+
+
+            return result;
+        }
+
+
+        private static void Main(string[] args)
+        {
+            var input = ReadLine();
+            WriteLine(BiggerIsGreater(input));
+        }
+    }
+}
